0b521913c3253aad03430fcbd8a052076aa43b34
Junnama Noda <junnama@alfasado.jp>
2015-10-13 17:11:05 +0900

    Added. New Tags.

--- plugins/BuildCache/lib/BuildCache/Plugin.pm
+++ plugins/BuildCache/lib/BuildCache/Plugin.pm
@@ -27,4 +27,33 @@ sub _hdlr_buildcache {
     }
 }
 
+sub _hdlr_if_cached_content {
+    my ( $ctx, $args, $cond ) = @_;
+    my $ttl = $args->{ ttl } or return '';
+    my $key = $args->{ key } or return '';
+    require MT::Request;
+    my $r = MT::Request->instance;
+    my $cache = $r->cache( 'buildcache:' . $key );
+    return 1 if $cache;
+    require MT::Cache::Negotiate;
+    my $driver = MT::Cache::Negotiate->new( ttl => $ttl );
+    my $value = $driver->get( $key );
+    if ( $value ) {
+        if (! Encode::is_utf8( $value ) ) {
+            Encode::_utf8_on( $value );
+        }
+        $r->cache( 'buildcache:' . $key, $value );
+        $ctx->stash( 'cached_content:' . $key, $value );
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+sub _hdlr_cached_content {
+    my ( $ctx, $args, $cond ) = @_;
+    my $key = $args->{ key } or return '';
+    return $ctx->stash( 'cached_content:' . $key );
+}
+
 1;
--- plugins/BuildCache/php/config.php
+++ plugins/BuildCache/php/config.php
@@ -6,9 +6,62 @@ class BuildCache extends MTPlugin {
         'id'   => 'BuildCache',
         'key'  => 'buildcache',
         'tags' => array(
-            'block' => array( 'buildcache' => '_hdlr_buildcache' ),
+            'block' => array( 'buildcache' => '_hdlr_buildcache',
+                              'cachecontent' => '_hdlr_cache_content',
+                              'ifcachedcontent' => '_hdlr_if_cached_content' ),
+            'function' => array( 'cachedcontent' => '_hdlr_cached_content',
+                                 'cachedcdata' => '_hdlr_cached_content' ),
         ),
     );
+
+    function _hdlr_cached_content ( $args, &$ctx ) {
+        $app = $ctx->stash( 'bootstrapper' );
+        $key = $args[ 'key' ];
+        $key = $app->escape( $key );
+        $cache = $ctx->stash( 'buildcache:' . $key );
+        return $cache;
+    }
+
+    function _hdlr_cache_content ( $args, $content, &$ctx, &$repeat ) {
+        if ( isset( $content ) ) {
+            $app = $ctx->stash( 'bootstrapper' );
+            $ttl = $args[ 'ttl' ];
+            $key = $args[ 'key' ];
+            $ttl = $app->escape( $ttl );
+            $key = $app->escape( $key );
+            $session = $app->get_by_key( 'Session', array( 'id' => $key, 'kind' => 'CO' ) );
+            $ts = time();
+            $session->session_start = $ts;
+            $session->session_duration = $ts + $ttl;
+            $session->data = $content;
+            $session->Save();
+        }
+        return $content;
+    }
+
+    function _hdlr_if_cached_content ( $args, $content, &$ctx, &$repeat ) {
+        if (! isset( $content ) ) {
+            $app = $ctx->stash( 'bootstrapper' );
+            $args[ 'check_cache' ] = 1;
+            $ttl = $args[ 'ttl' ];
+            $key = $args[ 'key' ];
+            $ttl = $app->escape( $ttl );
+            $key = $app->escape( $key );
+            $session = $app->load( 'Session', array( 'id' => $key, 'kind' => 'CO' ), array( 'limit' => 1 ) );
+            if ( $session ) {
+                $limit = time() + $ttl;
+                if ( $session->start < $limit ) {
+                    $ctx->stash( 'buildcache:' . $key, $session->data );
+                    return $ctx->_hdlr_if( $args, $content, $ctx, $repeat, TRUE );
+                } else {
+                    $session->Remove();
+                }
+            }
+            return $ctx->_hdlr_if( $args, $content, $ctx, $repeat, FALSE );
+        } else {
+            return $ctx->_hdlr_if( $args, $content, $ctx, $repeat );
+        }
+    }
 
     function _hdlr_buildcache ( $args, $content, &$ctx, &$repeat ) {
         $app = $ctx->stash( 'bootstrapper' );
@@ -16,36 +69,30 @@ class BuildCache extends MTPlugin {
         $key = $args[ 'key' ];
         $ttl = $app->escape( $ttl );
         $key = $app->escape( $key );
-        $cache = $ctx->stash( 'buildcache:' . $key );
-        if ( isset( $cache ) ) {
-            return $cache;
-        }
-        require_once( 'class.mt_session.php' );
-        $session = new Session;
-        $where = "session_id = '{$key}'";// AND session_duration > '{$duration}'";
-        $extra = array( 'limit' => 1 );
-        $cache = $session->Find( $where, FALSE, FALSE, $extra );
-        $duration = time();
-        if ( isset( $cache ) ) {
-            if ( $cache->duration < $duration ) {
-                $repeat = FALSE;
-                $ctx->stash( 'buildcache:' . $key, $cache[ 0 ]->data );
-                return $cache[ 0 ]->data;
-            } else {
-                $cache->Remove();
+        if (! isset( $content ) ) {
+            $session = $app->load( 'Session', array( 'id' => $key, 'kind' => 'CO' ) );
+            if ( $session ) {
+                $session = $session[ 0 ];
+                $limit = time() + $ttl;
+                if ( $session->start < $limit ) {
+                    $ctx->stash( 'buildcache:' . $key, $session->data );
+                    $repeat = FALSE;
+                    echo $session->session_data;
+                    return;
+                } else {
+                    $session->Remove();
+                }
             }
         }
         if ( isset( $content ) ) {
-            $session->session_id = $key;
-            $session->session_kind = 'CO';
-            $session->session_start = $duration;
-            $session->session_duration = $duration + $ttl;
+            $session = $app->get_by_key( 'Session', array( 'id' => $key, 'kind' => 'CO' ) );
+            $ts = time();
+            $session->session_start = $ts;
+            $session->session_duration = $ts + $ttl;
             $session->data = $content;
             $session->Save();
-            $repeat = FALSE;
-            $ctx->stash( 'buildcache:' . $key, $content );
-            return $content;
         }
+        return $content;
     }
 }
 
